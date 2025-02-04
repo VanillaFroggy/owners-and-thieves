a3fdc1ea41b57c19c0012a28ead65ff64bda4291
FrolovIV <FrolovIV@git.mail.com>
2025-02-04 16:08:44 +0300

    now thieves come to apartments only once

--- src/main/java/com/internship/model/Thief.java
+++ src/main/java/com/internship/model/Thief.java
@@ -3,10 +3,6 @@ package com.internship.model;
 public record Thief(Apartment apartment, Backpack backpack) implements Runnable {
     @Override
     public void run() {
-        while (true) {
-            if (apartment.stealItems(backpack().maxWeight()).isEmpty()) {
-                break;
-            }
-        }
+        apartment.stealItems(backpack().maxWeight());
     }
 }
