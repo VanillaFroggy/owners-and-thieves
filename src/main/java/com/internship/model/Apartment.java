8e6f52f4663389fb8cee0ba668144fd637e7226c
FrolovIV <FrolovIV@git.mail.com>
2025-02-05 10:18:47 +0300

    migrated from ArrayList to CopyOnWriteArrayList and owners adding items in shuffled order now

--- src/main/java/com/internship/model/Apartment.java
+++ src/main/java/com/internship/model/Apartment.java
@@ -3,26 +3,42 @@ package com.internship.model;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.util.*;
+import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class Apartment {
     private static final String NO_ITEMS_TO_STEAL_PHRASE = "Here is nothing to steal for ";
-    private final List<Item> items = new ArrayList<>();
+    private final List<Item> items = new CopyOnWriteArrayList<>();
     private final Lock lock = new ReentrantLock();
     private final Condition canSteal = lock.newCondition();
-    private boolean ownerInside = false;
+    private int ownersInside = 0;
+    private boolean thiefInside = false;
 
     public void addItem(Item item) {
         lock.lock();
         try {
-            ownerInside = true;
-            items.add(item);
-            System.out.println(Thread.currentThread().getName() + " added " + item);
+            while (thiefInside) {
+                canSteal.await();
+            }
+            ownersInside++;
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
         } finally {
-            ownerInside = false;
-            canSteal.signalAll();
+            lock.unlock();
+        }
+
+        items.add(item);
+        System.out.println(Thread.currentThread().getName() + " added " + item);
+
+        lock.lock();
+        try {
+            ownersInside--;
+            if (ownersInside == 0) {
+                canSteal.signalAll();
+            }
+        } finally {
             lock.unlock();
         }
     }
@@ -30,41 +46,50 @@ public class Apartment {
     public List<Item> stealItems(int maxWeight) {
         lock.lock();
         try {
-            while (ownerInside) {
+            while (ownersInside > 0 || thiefInside) {
                 canSteal.await();
             }
+            thiefInside = true;
             if (items.isEmpty()) {
+                thiefInside = false;
+                canSteal.signalAll();
                 System.out.println(NO_ITEMS_TO_STEAL_PHRASE + Thread.currentThread().getName());
                 return Collections.emptyList();
             }
-            items.sort(Comparator.comparing(item ->
-                    (item.value()
-                            .divide(
-                                    BigDecimal.valueOf(item.weight()),
-                                    RoundingMode.FLOOR
-                            )
-                    ).negate()
-            ));
-            List<Item> stolen = new ArrayList<>();
-            int currentWeight = 0;
-            for (Iterator<Item> iterator = items.iterator(); iterator.hasNext();) {
-                Item item = iterator.next();
-                if (currentWeight + item.weight() <= maxWeight) {
-                    stolen.add(item);
-                    currentWeight += item.weight();
-                    iterator.remove();
-                }
-            }
-            System.out.println(
-                    stolen.isEmpty()
-                            ? NO_ITEMS_TO_STEAL_PHRASE + Thread.currentThread().getName()
-                            : Thread.currentThread().getName() + " stole " + stolen
-            );
-            return stolen;
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
+        } catch (InterruptedException ex) {
+            throw new RuntimeException(ex);
         } finally {
             lock.unlock();
         }
+        items.sort(Comparator.comparing(item ->
+                (item.value()
+                        .divide(
+                                BigDecimal.valueOf(item.weight()),
+                                RoundingMode.FLOOR
+                        )
+                ).negate()
+        ));
+        List<Item> stolen = new ArrayList<>();
+        int currentWeight = 0;
+        for (Item item : items) {
+            if (currentWeight + item.weight() <= maxWeight) {
+                stolen.add(item);
+                currentWeight += item.weight();
+            }
+        }
+        items.removeAll(stolen);
+        System.out.println(
+                stolen.isEmpty()
+                        ? NO_ITEMS_TO_STEAL_PHRASE + Thread.currentThread().getName()
+                        : Thread.currentThread().getName() + " stole " + stolen
+        );
+        lock.lock();
+        try {
+            thiefInside = false;
+            canSteal.signalAll();
+        } finally {
+            lock.unlock();
+        }
+        return stolen;
     }
 }
